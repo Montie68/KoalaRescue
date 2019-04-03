376f442a3a71700c8040c046f834299079176c4b
Montie68 <geoff@crystalclearit.com.au>
2019-04-03 16:43:21 +1100

    3/4/19 Progress update

--- Assets/Plugins/Quick/Editor/Utils/QUtils.cs
+++ Assets/Plugins/Quick/Editor/Utils/QUtils.cs
@@ -198,10 +198,8 @@ namespace QuickEditor
                 case BuildTarget.WSAPlayer: return BuildTargetGroup.WSA; //Build an Windows Store Apps player.
                 case BuildTarget.StandaloneLinux64: return BuildTargetGroup.Standalone; //Build a Linux 64 - bit standalone.
                 case BuildTarget.StandaloneLinuxUniversal: return BuildTargetGroup.Standalone; //Build a Linux universal standalone.
-                case BuildTarget.PSP2: return BuildTargetGroup.PSP2; //Build a PS Vita Standalone.
                 case BuildTarget.PS4: return BuildTargetGroup.PS4; //Build a PS4 Standalone.
                 case BuildTarget.XboxOne: return BuildTargetGroup.XboxOne; //Build a Xbox One Standalone.
-                case BuildTarget.N3DS: return BuildTargetGroup.N3DS; //Build to Nintendo 3DS platform.
 
 #if !UNITY_2018_1_OR_NEWER
                 case BuildTarget.WiiU: return BuildTargetGroup.WiiU; //Build a Wii U standalone.
