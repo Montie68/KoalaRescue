312d01ee209ed10869fe4e218e275036a9a23ec8
Montie68 <geoff@crystalclearit.com.au>
2019-04-04 11:00:42 +1100

    BOD 4/4/19

---
+++ Assets/bin/Source/Enemy/EnemiesSpawner.cs
@@ -0,0 +1,26 @@
+﻿
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+namespace Enemies
+{
+    public class EnemiesSpawner : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+#if UNITY_EDITOR
+
+#endif
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+
+}
