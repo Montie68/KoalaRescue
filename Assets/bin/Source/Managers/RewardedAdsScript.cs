5e182fd08f84d944929c186e37d011268efbded6
Montie68 <geoff.newman@immersive.video>
2021-09-15 00:12:03 +1000

    EOD  14/09

--- Assets/bin/Source/Managers/RewardedAdsScript.cs
+++ Assets/bin/Source/Managers/RewardedAdsScript.cs
@@ -4,101 +4,103 @@ using UnityEngine.UI;
 using UnityEngine.Advertisements;
 using MoreMountains.InfiniteRunnerEngine;
 
-[RequireComponent(typeof(Button))]
-[RequireComponent(typeof(LevelSelector))]
-public class RewardedAdsScript : MonoBehaviour, IUnityAdsListener
+public class RewardedAdsScript : MonoBehaviour, IUnityAdsLoadListener, IUnityAdsShowListener, IUnityAdsInitializationListener
 {
 
-   // private string gameId = "3145513";
+    [SerializeField] Button _showAdButton = null;
+    [SerializeField] string _androidAdUnitId = "Rewarded_Android";
+    [SerializeField] string _iOsAdUnitId = "Rewarded_iOS";
+    [SerializeField] string _androidAdGameId = "3444263";
+    [SerializeField] string _iOsAdGameId = "3444262";
+    string _adUnitId;
+    string _gameId;
+    [SerializeField] bool _isTesting;
 
-    public bool testMode = true;
-    [SerializeField] string gameId;
-
-
-    [SerializeField] Button myButton;
-    [SerializeField] LevelSelector levelSelector;
-    public int lives = 1;
-    public string myPlacementId = "rewardedVideo";
-
-    void Start()
+    void Awake()
     {
-        // Ads testing script
-#if UNITY_IOS
-            gameId = "3444262";
-#elif UNITY_ANDROID
-        gameId =  "3444263";
-#else
-        gameId =  "3444263";
-#endif
-
-        myButton = myButton ?? GetComponent<Button>();
-        levelSelector = levelSelector ?? GetComponent<LevelSelector>();
-        // Set interactivity to be dependent on the Placement’s status:
-        
-
-        // Map the ShowRewardedVideo function to the button’s click listener:
-        if (myButton) myButton.onClick.AddListener(ShowRewardedVideo);
-
-        // Initialize the Ads listener and service:
-        Advertisement.AddListener(this);
-        Advertisement.Initialize(gameId, testMode);
+        // Get the Ad Unit ID for the current platform:
+        _adUnitId = (Application.platform == RuntimePlatform.IPhonePlayer)
+            ? _iOsAdUnitId
+            : _androidAdUnitId;
+        _gameId = (Application.platform == RuntimePlatform.IPhonePlayer)
+            ? _iOsAdGameId
+            : _androidAdGameId;
+        //Disable button until ad is ready to show
+        _showAdButton.interactable = false;
+        Advertisement.Initialize(_gameId, _isTesting);
     }
-    private void OnEnable()
+    // Load content to the Ad Unit:
+    public void LoadAd()
     {
-        StartCoroutine(EnableContinue());
-    }
-    // Implement a function for showing a rewarded video ad:
-    private IEnumerator EnableContinue()
-    {
-        yield return new WaitForSeconds(1 / 3);
-        myButton.gameObject.SetActive(Advertisement.IsReady(myPlacementId));
-    }
-    void ShowRewardedVideo()
-    {
-        Advertisement.Show(myPlacementId);
-#if UNITY_WEBGL
-        levelSelector.Continue(lives);
-#endif
+        // IMPORTANT! Only load content AFTER initialization (in this example, initialization is handled in a different script).
+        Debug.Log("Loading Ad: " + _adUnitId);
+        Advertisement.Load(_adUnitId, this);
     }
 
-    // Implement IUnityAdsListener interface methods:
-    public void OnUnityAdsReady(string placementId)
+    // If the ad successfully loads, add a listener to the button and enable it:
+    public void OnUnityAdsAdLoaded(string adUnitId)
     {
-        // If the ready Placement is rewarded, activate the button: 
-        if (placementId == myPlacementId)
+        Debug.Log("Ad Loaded: " + adUnitId);
+
+        if (adUnitId.Equals(_adUnitId))
         {
-            myButton.interactable = true;
-        }
-#if UNITY_WEBGL
-        myButton.interactable = true;
-#endif
-    }
-
-    public void OnUnityAdsDidFinish( string placementId, ShowResult showResult)
-    {
-        // Define conditional logic for each ad completion status:
-        if (showResult == ShowResult.Finished)
-        {
-            // Reward the user for watching the ad to completion.
-            levelSelector.Continue(lives);
-        }
-        else if (showResult == ShowResult.Skipped)
-        {
-            // Do not reward the user for skipping the ad.
-        }
-        else if (showResult == ShowResult.Failed)
-        {
-            Debug.LogWarning("The ad did not finish due to an error.");
+            // Configure the button to call the ShowAd() method when clicked:
+            _showAdButton.onClick.AddListener(ShowAd);
+            // Enable the button for users to click:
+            _showAdButton.interactable = true;
         }
     }
 
-    public void OnUnityAdsDidError(string message)
+    // Implement a method to execute when the user clicks the button.
+    public void ShowAd()
     {
-        // Log the error.
+        // Disable the button: 
+        _showAdButton.interactable = false;
+        // Then show the ad:
+        Advertisement.Show(_adUnitId, this);
     }
 
-    public void OnUnityAdsDidStart(string placementId)
+    // Implement the Show Listener's OnUnityAdsShowComplete callback method to determine if the user gets a reward:
+    public void OnUnityAdsShowComplete(string adUnitId, UnityAdsShowCompletionState showCompletionState)
     {
-        // Optional actions to take when the end-users triggers an ad.
+        if (adUnitId.Equals(_adUnitId) && showCompletionState.Equals(UnityAdsShowCompletionState.COMPLETED))
+        {
+            Debug.Log("Unity Ads Rewarded Ad Completed");
+            // Grant a reward.
+
+            // Load another ad:
+            Advertisement.Load(_adUnitId, this);
+        }
+    }
+
+    // Implement Load and Show Listener error callbacks:
+    public void OnUnityAdsFailedToLoad(string adUnitId, UnityAdsLoadError error, string message)
+    {
+        Debug.Log($"Error loading Ad Unit {adUnitId}: {error.ToString()} - {message}");
+        // Use the error details to determine whether to try to load another ad.
+    }
+
+    public void OnUnityAdsShowFailure(string adUnitId, UnityAdsShowError error, string message)
+    {
+        Debug.Log($"Error showing Ad Unit {adUnitId}: {error.ToString()} - {message}");
+        // Use the error details to determine whether to try to load another ad.
+    }
+
+    public void OnUnityAdsShowStart(string adUnitId) { }
+    public void OnUnityAdsShowClick(string adUnitId) { }
+
+    void OnDestroy()
+    {
+        // Clean up the button listeners:
+        _showAdButton.onClick.RemoveAllListeners();
+    }
+
+    public void OnInitializationComplete()
+    {
+    }
+
+    public void OnInitializationFailed(UnityAdsInitializationError error, string message)
+    {
+        Debug.LogErrorFormat("An Error occurred {0}: {1}",  message, error);
     }
 }
